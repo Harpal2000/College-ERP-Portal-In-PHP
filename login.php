7b2968def77145c4bb5519162d9ee9a53ba7b768
Harpal <rajanbabrah056@gmail.com>
2023-04-24 22:31:54 +0530

    add validations to login.php

--- login.php
+++ login.php
@@ -12,6 +12,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
     $user = $_POST['username'];
     $pass = $_POST['password'];
 
+    // if (empty($user) || empty($pass)) {
+    //     echo "<script>alert('Please enter both username and password'); window.location.href='login.php';</script>";
+    //     exit;
+    // }
+
+
     $Query = "select * from login where username = '$user' and password = '$pass'";
     echo $Query;
 
@@ -46,10 +52,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
 <head>
     <title>Dashboard: Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <style>
         body {
-            margin-top: 20px;
-            background: #f6f9fc;
+            margin - top: 20px;
+            background: #cbd7e2;
+        }
+
+        #main-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
         }
 
         .account-block {
@@ -62,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
         }
 
         .account-block .overlay {
-            -webkit-box-flex: 1;
+            -webkit - box - flex: 1;
             -ms-flex: 1;
             flex: 1;
             position: absolute;
@@ -71,11 +86,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
             left: 0;
             right: 0;
             background-color: rgba(0, 0, 0, 0.4);
-                        /* background-color: white; */
+            /* background-color: white; */
         }
 
         .account-block .account-testimonial {
-            text-align: center;
+            text - align: center;
             color: #fff;
             position: absolute;
             margin: 0 auto;
@@ -90,25 +105,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
         }
 
         .btn-theme {
-            background-color: #337362;
+            background - color: #337362;
             border-color: #337362;
             color: #fff;
+        }
+
+        .form-control:focus {
+            box-shadow: none;
+        }
+
+        label.error {
+            color: red;
+            font-size: 0.8rem;
+            font-weight: 400;
+            margin-top: 5px;
+            width: 100%;
         }
     </style>
 </head>
 
 <body>
-    <div id="main-wrapper" class="container ">
-        <div class="row justify-content-center align-middle my-auto ">
+    <div id="main-wrapper" class="container">
+        <div class="row justify-content-center align-middle my-auto">
             <div class="col-xl-10">
                 <div class="card border-0">
                     <div class="card-body p-0">
                         <div class="row no-gutters">
                             <div class="col-lg-6 d-none d-lg-inline-block">
-                                <div class="account-block rounded-right " >
-                                    <!-- <div class="overlay rounded-right "></div> -->
-                                    <img src="/log.jpeg" alt="" width="500px" height="500x">
-                                    <!-- <img src="/images/bf.png" alt="" class="img-fluid "> -->
+                                <div class="account-block rounded-right">
+                                    <img src="/log.jpeg" alt="" class="img-fluid" width="390px" height="500x">
                                 </div>
                             </div>
                             <div class="col-lg-6 align-middle ">
@@ -122,7 +147,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
                                         panel.</p>
 
                                     <div id="regForm">
-                                        <form action="" method="POST">
+                                        <form id="loginForm" method="POST">
                                             <div class="form-group mb-4">
                                                 <label for="username">User Name</label>
                                                 <input type="text" class="form-control" name="username" id="username">
@@ -145,7 +170,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
                 </div>
             </div>
         </div>
-    </div>
+
+        <script>
+            $(document).ready(function () {
+                $('#loginForm').validate({
+                    rules: {
+                        username: {
+                            required: true,
+                        },
+                        password: {
+                            required: true,
+                        },
+                    },
+                    messages: {
+                        username: {
+                            required: 'Please enter your username.',
+                        },
+                        password: {
+                            required: 'Please enter your password.',
+                        },
+                    },
+                });
+            });
+        </script>
 </body>
 
 </html>
